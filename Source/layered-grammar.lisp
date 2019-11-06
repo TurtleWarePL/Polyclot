29ef0298826da2f65c4589756a29d892dfaf91b8
Daniel Kochmański <daniel@turtleware.eu>
2019-11-06 11:23:27 +0100

    Initial commit.
    - creates basic directory structure
    - adds system definition
    - provides preliminary doc (dataframes)
    - adds basic implementation of a data frame as specified
    
    Missing:
    
    - specification for data frame join operation
    - data frame viewer (print to text stream and clim stream)

---
+++ Source/layered-grammar.lisp
@@ -0,0 +1,8 @@
+(in-package #:eu.turtleware.polyclot.impl)
+
+(define-class <aesthetic> () ())
+(define-class <stat>      () ())
+(define-class <geom>      () ())
+(define-class <mods>      () ())
+(define-class <scale>     () ())
+(define-class <coord>     () ())
