7ef977410a9600c31fc6135ca1a1e5d988ef0992
Daniel Kochmański <daniel@turtleware.eu>
2019-11-15 19:35:37 +0100

    layered grammar: add incomplete implementation
    Missing:
    - documentation needs work
    - protocol is not documented
    - define-chart macro
    - <geom> class is embrionic now
    - <coords>, <scale> and <facet> are stubs
    - no tests at all

--- Source/layered-grammar.lisp
+++ Source/layered-grammar.lisp
@@ -1,8 +1,104 @@
 (in-package #:eu.turtleware.polyclot.impl)
 
-(define-class <aesthetic> () ())
-(define-class <stat>      () ())
-(define-class <geom>      () ())
-(define-class <mods>      () ())
-(define-class <scale>     () ())
-(define-class <coord>     () ())
+(define-class <aest> ()
+  (;; mapping
+   (aest :initarg :aest)
+   (vars :initarg :vars)
+   ;; slots used for caching
+   (last-df :initform nil)
+   (indexes :initform nil)))
+
+(define-class <stat> ()
+  ((aest :initarg :aest :reader aest)))
+
+;;; <mods> when used in a <stat> context is applied to all rows.
+(define-class <mods>     (<stat>) ())
+(define-class <identity> (<mods>) ())
+
+(define-class <geom> ()
+  ((aest :initarg :aest :reader aest)))
+
+;;; FIXME stubs for now
+(define-class <scale> () ())
+(define-class <coord> () ())
+(define-class <facet> () ())
+
+(define-class <layer> ()
+  ((data :initarg :data :reader data)
+   (aest :initarg :aest :reader aest)
+   (stat :initarg :stat :reader stat)
+   (geom :initarg :geom :reader geom)
+   (mods :initarg :mods :reader mods)))
+
+(define-class <chart> (<layer>)
+  ((coord :initarg :coord :reader coord)
+   (scale :initarg :scale :reader scale)
+   (facet :initarg :facet :reader facet)
+   (layer :initarg :layer :reader layer)))
+
+(defun map-aesthetics (aest df row)
+  (with-slots (aest vars last-df indexes) aest
+    (unless (eql last-df df)
+      (setf last-df df
+            indexes (map 'list
+                         (lambda (col)
+                           (position col (cols df) :test #'string=))
+                         vars)))
+    (loop for aes in aest
+          for ind in indexes
+          collect aes
+          collect (ref df row ind))))
+
+;;; CAUTION modifies a dataframe row
+(defun unmap-aesthetics! (aest df row vals)
+  (with-slots (aest vars last-df indexes) aest
+    (unless (eql last-df df)
+      (setf last-df df
+            indexes (map 'list
+                         (lambda (col)
+                           (position col (cols df) :test #'string=))
+                         vars)))
+    (loop for aes in aest
+          for ind in indexes
+          for val = (getf vals aes)
+          do (setf (elt row ind) val))))
+
+(defgeneric collision-modifier (<stat> vals)
+  (:method ((stat <identity>) vals)
+    vals))
+
+(defgeneric statistical-transformation (<stat> <data-frame>)
+  (:method ((stat <identity>) (frame <data-frame>))
+    (declare (ignore stat))
+    frame)
+  (:method ((stat <mods>) (frame <data-frame>))
+    (let ((df (copy-data-frame frame)))
+      (flet ((modify-row (index row)
+               (let* ((aest (aest stat))
+                      (vals (map-aesthetics aest df row))
+                      (modified-vals (collision-modifier stat vals)))
+                 (unmap-aesthetics! (aest stat) df row modified-vals))))
+        (map-data-frame-rows frame t #'modify-row)))))
+
+(defgeneric geometric-object-start  (<geom> <coord> <mods>))
+(defgeneric geometric-object-add    (<geom> &rest args))
+(defgeneric geometric-object-finish (<geom>))
+
+(defun aest (&rest plist)
+  (loop for (i j) on plist by #'cddr
+        collect i into aes
+        collect j into var
+        finally (return (<aest> :aest aes :vars var))))
+
+(defun stat (class &rest args)
+  (apply class args))
+
+(defun geom (class &rest args)
+  (apply class args))
+
+(defun mods (class &rest args)
+  (apply class args))
+
+;;; FIXME macro stub
+(defmacro define-chart (name options &rest layers)
+  (declare (ignore name options layers)))
---
+++ Source/statistical-transformations.lisp
@@ -0,0 +1,26 @@
+(in-package #:eu.turtleware.polyclot.impl)
+
+(define-class <count> (<stat>) ())
+
+(defmethod statistical-transformation ((stat <count>) (frame <data-frame>))
+  (let ((df (copy-data-frame frame))
+        (aest (aest stat))
+        (fr (make-hash-table :test #'equal)))
+    (prog1 df
+      (flet ((add-it (index row)
+               (declare (ignore index))
+               (let* ((vals (funcall aest df row))
+                      (xval (getf vals :x)))
+                 (incf (gethash xval fr 0))))
+             (put-it (index row)
+               (declare (ignore index))
+               (let* ((vals (map-aesthetics aest df row))
+                      (xval (getf vals :x)))
+                 (gethash xval fr 0))))
+        (map-data-frame-rows df t #'add-it)
+        (add-cols! df "..count.." #'put-it)))))
+
+(define-class <bin> (<stat>)
+  ;; :width and :count are mutually exclusive.
+  ((width :initarg :width)
+   (count :initarg :count)))
